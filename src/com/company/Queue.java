0548dca48d3d13844a5d38d37025b5a9f8abd080
Shreyansh <aayansrivastava1912@gmail.com>
2022-03-16 16:51:07 +0530

    Queue

---
+++ src/com/company/Queue.java
@@ -0,0 +1,64 @@
+package com.company;
+public class Queue {
+    static class Queue1 {
+        static int arr[];
+        static int size;
+        static int rear;
+
+        Queue1(int size) {
+            this.size = size;
+            arr = new int[size];
+            rear = -1;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+
+        public static boolean isFull() {
+            return rear == size - 1;
+        }
+
+        public static void add(int data) {
+            if (isFull()) {
+                System.out.println("Queue is full");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+        public static int peek(){
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+
+
+        public static void main(String[] args) {
+            Queue1 q = new Queue1(5);
+            q.add(1);
+            q.add(2);
+            q.add(3);
+            while(!q.isEmpty()){
+                System.out.println(q.peek());
+                q.remove();
+
+            }
+        }
+    }
+}
